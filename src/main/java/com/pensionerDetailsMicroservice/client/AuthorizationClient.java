19c130e21725c10f6f05915613208dead4c49eba
Swadesh Nayak <swadeshnayak123@gmail.com>
2022-08-22 10:05:17 +0530

    Update AuthorizationClient.java

--- src/main/java/com/pensionerDetailsMicroservice/client/AuthorizationClient.java
+++ src/main/java/com/pensionerDetailsMicroservice/client/AuthorizationClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 
 // @FeignClient(name = "auth-service",url = "http://demo-lb-19644722.ap-south-1.elb.amazonaws.com/auth")
-@FeignClient(name = "auth-service",url = "http://13.235.87.33:8081/auth")
+@FeignClient(name = "auth-service",url = "http://65.0.80.86:8081/auth")
 public interface AuthorizationClient {
 	@PostMapping(value = "/authorize")
 	public boolean authorizeRequest(@RequestHeader(value = "Authorization",required = true) String header);
